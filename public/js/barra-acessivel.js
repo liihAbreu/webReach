41251e3e94ab220312a59efa11a63dbca5bc83d7
liliane06 <liliane@happybiz.com.br>
2019-05-05 03:03:36 -0300

    alter

--- public/js/barra-acessivel.js
+++ public/js/barra-acessivel.js
@@ -2,8 +2,11 @@
     var fontes = ['font-1x','font-2x','font-3x','font-4x'];
 
     $('#fonte-maior').click(function(){
-        for(var i= 0; i <= fontes.length; i++){
-            var fonteAtual = fontes[0+1]; 
+        for(var i= 0; i < fontes.length; i++){
+            var fonteAtual = i;
+            if(fontes[0]){
+                fonteAtual = fontes[i+1];
+            } 
         }
         console.log(fonteAtual);
         
