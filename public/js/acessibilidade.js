ca53a3ab1f87b764da37e2bcc9f5da894234b3d3
liliane06 <liliane@happybiz.com.br>
2019-05-05 20:44:06 -0300

    alter

--- public/js/acessibilidade.js
+++ public/js/acessibilidade.js
@@ -6,8 +6,6 @@
 
     function Acessibilidade(){
         // o controle do thema sera dado pelo body
-        this.body = document.querySelector('body');
-        this.themas = ['preto', 'amarelo', 'default'];
         this.focoDivs = {
             acima: document.querySelector('.foco-leitura'),
             abaixo: document.querySelector('.segundo-foco')
@@ -20,91 +18,9 @@
             foco: document.getElementById('foco'),
         }
 
-        //botões para controle do nome
-        this.btnThemas = document.getElementsByClassName('thema');
-
-        //botões para o controle das fontes
-        this.maiorBtn = document.getElementById('fonte-maior');
-        this.menorBtn = document.getElementById('fonte-menor');
-
-        //controlar o tamanho da fonte
-        this.fonts = ['font-1x', 'font-2x', 'font-3x', 'font-4x'];
-        
-        // verefica se já existe a font definida pelo usúario no Storage
-        this.fontAtual = parseInt( localStorage.getItem('fontUsuario') );
-        if(!this.fontAtual){
-            localStorage.setItem('fontUsuario', 0);
-            this.fontAtual = 0;
-        }else{
-            this.aumentarFont(parseInt(this.fontAtual));
-            
-        }
-
-        // verefica se já existe um thema definido pelo usuario
-        this.thema = localStorage.getItem('thema');
-        if(!this.thema){
-            localStorage.setItem("thema", "default");
-            this.thema = "default";
-        }else{
-            this.trocarThema(this.thema);
-        }
-
-        this.inicio();
-    }
-
     Acessibilidade.prototype = {
         inicio: function(){
             this.eventos();
-        },
-        trocarThema: function(el){
-            var self = this;
-            if( typeof el === "object"){
-                var thema = el.dataset.thema;
-            }else{
-                var thema = el;
-                el = $('[data-thema="'+el +'"]');
-            }            
-
-            this.themas.forEach(function(th, i){
-                self.body.classList.remove(th);
-            });
-            
-            Array.prototype.forEach.call(this.btnThemas, function(th, i) { 
-                th.classList.remove('ativo');
-            });
-
-            $(el).addClass('ativo');
-            this.body.classList.add(thema);
-
-            localStorage.setItem("thema", thema);
-
-        },
-        aumentarFont: function(e){
-            if(typeof e === 'number'){
-                document.getElementsByTagName('body')[0].classList.add(this.fonts[this.fontAtual]);
-                sameSize($('.same-size-dep'));
-                return
-            }
-            var qual = typeof e === 'number' ? parseInt(e): parseInt( $(e).data('font') );
-            this.fontAtual += parseInt(qual);
-
-            if(this.fontAtual < 0 )
-            {
-                this.fontAtual = 0;
-                return
-            }else if ( this.fonts.length -1< this.fontAtual ){
-                this.fontAtual = this.fonts.length;
-                return
-            }                
-
-            Array.prototype.forEach.call(this.fonts, function(font, i) { 
-                document.getElementsByTagName('body')[0].classList.remove(font);
-            });
-
-            document.getElementsByTagName('body')[0].classList.add(this.fonts[this.fontAtual]);
-            sameSize($('.same-size-dep'));
-            
-            localStorage.setItem('fontUsuario', this.fontAtual);
         },
         eventos: function(){
             var self = this;
--- public/js/barra-acessivel.js
+++ public/js/barra-acessivel.js
@@ -1,15 +1,11 @@
 (function($){
-    var fontes = ['font-1x','font-2x','font-3x','font-4x'];
-
     $('#fonte-maior').click(function(){
-        for(var i= 0; i < fontes.length; i++){
-            var fonteAtual = i;
-            if(fontes[0]){
-                fonteAtual = fontes[i+1];
-            } 
-        }
-        console.log(fonteAtual);
+        $('body').addClass('font-2x');
+        console.log('ok');
         
     });
-
+    
+    $('#fonte-menor').click(function(){
+        $('body').removeClass('font-2x');
+    });   
 })($)
